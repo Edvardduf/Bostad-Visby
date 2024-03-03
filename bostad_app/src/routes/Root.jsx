4c4ea9a44ef7f15993b49595505450a2db420fd4
Edvard Dufvenmark <edvardduf@gmail.com>
2024-03-03 22:13:46 +0100

    Configured shadcn and did some testing

--- bostad_app/src/routes/Root.jsx
+++ bostad_app/src/routes/Root.jsx
@@ -1,9 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { CarouselDemo } from "@/components/demo/carouseldemo";
+
 export default function Root() {
   return (
     <>
-      <div>
-        <h2 className="bg-red-500 text-center text-2xl">Bostad visby</h2>
+      <div className="flex justify-center">
+    <Button>Test knapp med shadcn</Button>
       </div>
+    <div className="flex justify-center pt-5">
+    <CarouselDemo></CarouselDemo>
+
+    </div>
     </>
   );
 }
