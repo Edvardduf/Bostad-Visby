c7714813fa48183f9f827f6cf7fa6ad637ad74ed
Raymond Jones <raymondj@MBPsomtrRaymond.lan>
2024-03-05 00:15:38 +0100

    Created 2 landing pages/navbar sections for A/B testing

--- bostad_app/src/App.jsx
+++ bostad_app/src/App.jsx
@@ -1,8 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
 
   return (
     <>
       <div>
+        <Navbar></Navbar>
         <h2 className="bg-red-500 text-center text-2xl">Bostad visby</h2>
       </div>
     </>
