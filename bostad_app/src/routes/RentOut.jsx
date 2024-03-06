58067750c4ab3896446e3e3d6988cbd26db93a1d
Edvard Dufvenmark <edvardduf@gmail.com>
2024-03-06 23:45:05 +0100

    Made a new hero section and logo, but still work in progress

--- bostad_app/src/routes/RentOut.jsx
+++ bostad_app/src/routes/RentOut.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function RentOut() {
   return (
     <div
-      className="bg-cover h-screen"
-      style={{ backgroundImage: "url('/almedalen-visby.jpg')" }}
+      className="h-4/5"
+      style={{ backgroundImage: "url('/almedalen-visby.jpg')",  filter: 'saturate(150%) contrast(90%) brightness(90%) sepia(5%) hue-rotate(10deg) opacity(100%)' }}
     >
-      <ul className="flex space-x-7 text-white p-5 items-center font-serif text-lg">
-        <img src="/bostad-visby-logo.png" alt="logo" />
-        <li className="hover:underline">Hem</li>
+      <ul className="flex space-x-7 text-white p-5 items-center font-serif text-lg bg-blend-saturation">
+        <li className="hover:underline text-7xl text-bold text-center">Bostad Visby</li>
+        <li className="hover:underline text-pretty text-bold">Hem</li>
         <li className="hover:underline">Kontakt</li>
         <li className="hover:underline">Köp</li>
       </ul>
