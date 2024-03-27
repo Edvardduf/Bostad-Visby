b7f7f73ff3fe4b955e4ffc12a6337d5f78b6b67b
Edvard Dufvenmark <edvardduf@gmail.com>
2024-03-27 14:29:09 +0100

    Tried out a formik form

--- bostad_app/src/components/ListingsForm.jsx
+++ bostad_app/src/components/ListingsForm.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import RentersForm from "./RenterForm";
 
 function debounce(func, wait) {
-    let timeout;
-    return function executedFunction(...args) {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            func(...args);
-        }, wait);
-    };
+  let timeout;
+  return function executedFunction(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...args);
+    }, wait);
+  };
 }
 // Simulated fetch functions for relationship fields
 const fetchPropertyTypes = async () => {
@@ -65,9 +65,25 @@ const ListingForm = () => {
   const [renterExists, setRenterExists] = useState(null);
   const [showRenterModal, setShowRenterModal] = useState(false);
 
-
   const toggleRenterModal = () => {
     setShowRenterModal(!showRenterModal);
+  };
+  const checkRenterExistence = async (email) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8000/renter/${encodeURIComponent(email)}`
+      );
+      if (!response.ok) throw new Error("Network response was not ok");
+      const exists = await response.json();
+      if (!exists) {
+        setTimeout(() => setShowRenterModal(true), 500); // Delay showing the modal
+      } else {
+        setShowRenterModal(false);
+      }
+    } catch (error) {
+      console.error("Error checking renter existence:", error);
+      setShowRenterModal(false);
+    }
   };
 
   const handleChange = (e) => {
@@ -79,28 +95,9 @@ const ListingForm = () => {
     }
     if (name === "renterEmail") {
       // Delay the existence check to avoid too frequent checks
-      setTimeout(() => checkRenterExistence(value), 300);
+      setTimeout(() => checkRenterExistence(value), 500);
     }
   };
-
-  const checkRenterExistence = async (email) => {
-    try {
-        const response = await fetch(`http://localhost:8000/renter/${encodeURIComponent(email)}`);
-        if (!response.ok) throw new Error('Network response was not ok');
-        const exists = await response.json();
-        if (!exists) {
-            setTimeout(() => setShowRenterModal(true), 500); // Delay showing the modal
-        } else {
-            setShowRenterModal(false);
-        }
-    } catch (error) {
-        console.error("Error checking renter existence:", error);
-        setShowRenterModal(false);
-    }
-};
-
-const debounceTimeoutRef = useRef();
-
   const handleImageChange = (e) => {
     const files = Array.from(e.target.files);
     setFormData((prevState) => ({
@@ -117,11 +114,13 @@ const debounceTimeoutRef = useRef();
   };
 
   return (
-    <div className=" ml-4 rounded-lg shadow-lg max-w-3xl mx-auto my-8"
-    style={{
+    <div
+      className=" ml-4 rounded-lg shadow-lg max-w-3xl mx-auto my-8"
+      style={{
         background:
           "linear-gradient(to bottom,#EAFAF1 , #EBF5FB, #E8F8F5, #E8F6F3, #D0ECE7, #D1F2EB, #EAFAF1 )",
-      }}>
+      }}
+    >
       <form
         onSubmit={handleSubmit}
         className="space-y-6 p-4  shadow rounded-lg"
@@ -134,15 +133,15 @@ const debounceTimeoutRef = useRef();
             Email
           </label>
           <input
-    type="email"
-    name="renterEmail"
-    id="renterEmail"
-    value={formData.renterEmail}
-    onChange={handleChange}
-    onBlur={() => debouncedCheckRenterExistence(formData.renterEmail)}
-    className="mt-1 py-1 px-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-    required
-/>
+            type="email"
+            name="renterEmail"
+            id="renterEmail"
+            value={formData.renterEmail}
+            onChange={handleChange}
+            onBlur={formData.renterEmail}
+            className="mt-1 py-1 px-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+            required
+          />
         </div>
         <div>
           <label
@@ -194,7 +193,7 @@ const debounceTimeoutRef = useRef();
             htmlFor="address"
             className="block text-base font-medium text-black"
           >
-           Adress till objektet
+            Adress till objektet
           </label>
           <input
             type="text"
@@ -304,7 +303,7 @@ const debounceTimeoutRef = useRef();
             id="neighborhood_id"
             value={formData.neighborhood_id}
             onChange={handleChange}
-            className="mt-2 py-3 px-2 block w-full rounded-md text-gray-500 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 sm:text-sm"
+            className="mt-2 py-3 px-2 block w-full rounded-md text-gray-500 border-gray-300 shadow-sm focus:border-blue-200 focus:ring focus:ring-blue-300 focus:ring-opacity-50 sm:text-sm"
           >
             <option value="">Select Neighborhood</option>
             {neighborhoods.map((neighborhood) => (
@@ -342,46 +341,63 @@ const debounceTimeoutRef = useRef();
         </div>
         <button
           type="submit"
-          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"        >
+          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
           Submit Listing
         </button>
       </form>
       {showRenterModal && (
-    <div className="fixed inset-0 z-10 overflow-y-auto">
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+            >
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
 
             {/* This element is to trick the browser into centering the modal contents. */}
-            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-            
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div className="sm:flex sm:items-start">
-                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                            <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                                Renter Information
-                            </h3>
-                            <div className="mt-2">
-                                <RentersForm onCreate={() => {}} renterEmail={formData.renterEmail} />
-                            </div>
-                        </div>
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3
+                      className="text-lg leading-6 font-medium text-gray-900"
+                      id="modal-title"
+                    >
+                      Renter Information
+                    </h3>
+                    <div className="mt-2">
+                      <RentersForm
+                        onCreate={() => {}}
+                        renterEmail={formData.renterEmail}
+                      />
                     </div>
+                  </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-black hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={toggleRenterModal}>
-                        Close
-                    </button>
-                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-black hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={toggleRenterModal}
+                >
+                  Close
+                </button>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-)}
+      )}
     </div>
   );
 };
-
-
 
 export default ListingForm;
